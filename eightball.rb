c3ad64802be514e1547ce91fddf87dfe282f920d
Alan Vincent <alan.d.vincent@gmail.com>
2017-05-18 14:03:47 -0600

    working magic8ball with no easter eggs

--- eightball.rb
+++ eightball.rb
@@ -12,19 +12,50 @@
     # example: ruby eightball.rb add_answers
     # this would start your script but instead of running regular you would be prompted to add answers first
 
+stock_answers = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes Definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most Likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+]
+@stock_answers_clone = stock_answers.clone
 
+def get_stock_answers
+    puts "The answer to your question #{@question} is"
+    puts "\n\n*************************"
+    puts @stock_answers_clone.sample
+    puts "*************************"
+    puts "\n\n"
+    main_menu
+end
 
 def main_menu
     puts "---Welcome to the Magic 8 ball---"
     puts "\n\nEnter a question or type 'quit' to exit"
     print "> "
     
-    question = gets.chomp
+    @question = gets.chomp
 
-        if question == "quit"
+        if @question == "quit"
             exit
         else
-          # play in the array!
+          get_stock_answers
         end
 end
 
