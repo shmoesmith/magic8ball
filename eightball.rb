6b8394a1bc0216de57423afe40c90c7069ce8c9b
Alan Vincent <alan.d.vincent@gmail.com>
2017-05-18 16:05:37 -0600

    added secret menu with easter eggs, defined methods custom answers, custom reset, stock answers

--- eightball.rb
+++ eightball.rb
@@ -1,6 +1,3 @@
-# basic objectives 1.)user inputs question, 2.) computer outputs random answer 3.) user inputs 'quit'
-# 4.)computer outputs goodbye message and exits
-
 #bonus 1. ability to add more questions via 1a. via easter eggs questions 2a. do not let them add the same 
 #answer if its already in database
 
@@ -8,11 +5,8 @@
 
 #bonus 3. ability to have eightball print all answers 3a. via easter egg question
 
-#bonus 4. ability to use script arguments when the eightball is started to do functionality above
-    # example: ruby eightball.rb add_answers
-    # this would start your script but instead of running regular you would be prompted to add answers first
 
-stock_answers = [
+@stock_answers = [
     "It is certain",
     "It is decidedly so",
     "Without a doubt",
@@ -34,7 +28,32 @@ stock_answers = [
     "Outlook not so good",
     "Very doubtful",
 ]
-@stock_answers_clone = stock_answers.clone
+@stock_answers_clone = @stock_answers
+
+def custom_answers
+    @stock_answers_clone = []
+    puts "1. Enter a custom answer"
+    puts "2. Return to menu"
+
+    push_or_return = $stdin.gets.chomp
+
+    case push_or_return
+        when "1"
+            #do something
+        when "2"
+            secret_menu
+        else
+            puts "enter a valid selection"
+    end
+end
+
+
+def custom_reset
+    @stock_answers_clone = @stock_answers
+    puts "\nAnswers reset to stock!\n"
+    secret_menu
+end
+
 
 def get_stock_answers
     puts "The answer to your question #{@question} is"
@@ -45,19 +64,76 @@ def get_stock_answers
     main_menu
 end
 
+
 def main_menu
     puts "---Welcome to the Magic 8 ball---"
+    puts " \t _......._
+      .-:::::::::::-.
+    .:::::::::::::::::.
+   :::::::' .-. `:::::::
+  :::::::  :   :  :::::::
+ ::::::::  :   :  ::::::::
+ :::::::::._`-'_.:::::::::
+ :::::::::' .-. `:::::::::
+ ::::::::  :   :  ::::::::
+  :::::::  :   :  :::::::
+   :::::::._`-'_.:::::::
+    `:::::::::::::::::'
+      `-:::::::::::-'
+         `'''''''`"
     puts "\n\nEnter a question or type 'quit' to exit"
     print "> "
     
-    @question = gets.chomp
+    @question = $stdin.gets.chomp
 
         if @question == "quit"
+            puts "\n\n\nTHANKS FOR PLAYING! GOODBYE\n\n\n"
             exit
+        elsif @question == "secret"
+            secret_menu
         else
           get_stock_answers
         end
 end
 
-main_menu
+
+def secret_menu
+    puts "---Welcome to the super secret Magic 8 ball menu---"
+    puts "\nThis menu will allow you to do all sorts of neat things!"
+    puts "\n1. Add some some sick custom answers!"
+    puts "2. Reset answers back to stock answers"
+    puts "3. See all current Magic 8 Ball answers"
+    puts "4. Play Magic 8 Ball"
+    puts "\nType 'quit' to exit\n"
+    print "> "
+
+    secret_menu_answer = $stdin.gets.chomp
+
+    case secret_menu_answer
+        when "1"
+            custom_answers
+        when "2"
+           custom_reset
+        when "3"
+            @stock_answers_clone.each {|answer| puts answer}
+            puts "\n\n"
+            secret_menu
+        when "4"
+            main_menu
+        when "quit"
+            exit
+        else
+            puts "Please enter a valid selection"
+
+        end
+end
+
+
+secret_menu_parameter = ARGV.first
+
+    if secret_menu_parameter == "secret"
+        secret_menu
+    else
+        main_menu
+    end
 
